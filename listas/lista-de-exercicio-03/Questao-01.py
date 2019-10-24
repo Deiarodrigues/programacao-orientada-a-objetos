f05bb37b80b1453c59ebb5a0c52bc03c22f4f245
deiarodrigues <deiarodrigues136@gmail.com>
2019-10-24 09:47:47 -0300

    adicioneias primeiras questoes da lista 03

---
+++ listas/lista-de-exercicio-03/Questao-01.py
@@ -0,0 +1,6 @@
+numero1 = int(input("Digite um numero: "))
+numero2 = int(input("Digite outro numero: "))
+if numero1>numero2:
+	print("o maior foi {}".format(numero1))
+else:
+	print("o maior foi {}".format(numero2))
