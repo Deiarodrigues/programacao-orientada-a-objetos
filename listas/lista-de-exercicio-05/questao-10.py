c180d79904fb91fea51750a3d08f290b7a8d5e6f
deiarodrigues <deiarodrigues136@gmail.com>
2019-11-25 21:04:35 -0200

    adicionando questao

---
+++ listas/lista-de-exercicio-05/questao-10.py
@@ -0,0 +1,35 @@
+from random import randint
+jogo = 0
+ 
+while True:
+    jogada = int(input('digite 1 para jogar o dado: '))
+    jogo +=1
+    while jogo ==1:
+        dados = randint(2,12)
+        print(dados)
+        if dados == 7 or dados==11:
+            print('Voce é um jogador Natural!!!\nPARABENS!!!')
+            print(dados)
+                            
+        elif dados == 2 or dados ==3 or dados == 12:
+            print(dados)
+            print('CRAPS:(\nVoce perdeu!')
+        else:
+            print(dados)
+            break
+        
+        if dados == 4 or dados == 5 or dados ==6 or dados==8 or dados == 9 or dados ==10:
+            jogo+=1
+            print('Este é seu ponto, seu objetivo é jogar os dados ate tirar esse numero novamente!!')
+            print(dados)
+
+    while jogo == 2:
+        if dados == 7:
+            print('voce perdeu')
+            print(dados)
+            
+        else:
+            print(dados)
+            break
+
+            continue
